eac6d97ae863630a71b3a2f8272409d71f220ff0
Abdulaziz930 <abdulazizak@code.edu.az>
2021-03-09 01:14:41 +0400

    responsive header is done

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
-// let icon = document.querySelector("i");
-// let siderBar = document.querySelector(".side-bar");
-
-// icon.addEventListener("click", function () {
-//   siderBar.style.transition = "all 0.5s";
-//   if (siderBar.classList.contains("active")) {
-//     siderBar.classList.remove("active");
-//     siderBar.style.width = "0px";
-//   } else {
-//     siderBar.classList.add("active");
-//     siderBar.style.width = "300px";
-//   }
-// });
-
-let searchBtn = document.querySelector(".fa-search");
+let searchBtn = document.querySelector(".searchBtn");
 let closeBtn = document.querySelector(".fa-times");
 let searchLink = document.querySelector(".search-link");
 let searchDropDown = document.querySelector(".right-dropdown");
 let searchInp = document.querySelector("input[type='search']");
+let barIcon = document.querySelector(".fa-bars");
+let hamburgerMenu = document.querySelector(".left-side");
+let navbar = document.querySelector(".nav-bottom-item");
+let firstBadge = document.querySelector(".first-badge");
+let secondBadge = document.querySelector(".second-badge");
+let lastBadge = document.querySelector(".last-list-item");
+
+window.addEventListener("scroll", function () {
+  navbar.style.transition = "all 0.5s";
+  if (window.pageYOffset > "100") {
+    navbar.style.position = "fixed";
+    navbar.style.width = "100%";
+    navbar.style.top = "0";
+    firstBadge.classList.add("first-badge-1");
+    secondBadge.classList.add("second-badge-1");
+    lastBadge.classList.add("last-list-item-1");
+  } else {
+    navbar.style.position = "";
+    firstBadge.classList.remove("first-badge-1");
+    secondBadge.classList.remove("second-badge-1");
+    lastBadge.classList.remove("last-list-item-1");
+  }
+});
 
 searchLink.addEventListener("click", function () {
   if (searchBtn.style.display === "none") {
@@ -32,4 +41,17 @@ searchLink.addEventListener("click", function () {
 
 searchInp.addEventListener("blur", function () {
   searchInp.classList.add("blur");
+});
+
+barIcon.addEventListener("click", function () {
+  hamburgerMenu.style.transition = "all 0.5s";
+  if (hamburgerMenu.classList.contains("active")) {
+    hamburgerMenu.classList.remove("active");
+    hamburgerMenu.style.width = "0px";
+    hamburgerMenu.style.opacity = "0";
+  } else {
+    hamburgerMenu.classList.add("active");
+    hamburgerMenu.style.width = "140px";
+    hamburgerMenu.style.opacity = "1";
+  }
 });
