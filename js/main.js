dfa24144404ee546eb8620ac7aca57b5c98f31af
Abdulaziz930 <abdulazizak@code.edu.az>
2021-03-11 17:29:10 +0400

    newsletter section is done

--- js/main.js
+++ js/main.js
@@ -10,7 +10,10 @@ let firstBadge = document.querySelector(".first-badge");
 let secondBadge = document.querySelector(".second-badge");
 let lastBadge = document.querySelector(".last-list-item");
 let productIconBox = document.querySelectorAll(".product-icon");
+let mailInp = document.querySelector("input[type='email']");
 let productIcon = document.querySelectorAll(".fa-heart");
+let newsletterForm = document.querySelector(".newsletter-form");
+let mailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const nextIcon = '<i class="fas fa-chevron-right"></i>';
 const prevIcon = '<i class="fas fa-chevron-left"></i>';
 
@@ -47,6 +50,10 @@ searchInp.addEventListener("blur", function () {
   searchInp.classList.add("blur");
 });
 
+mailInp.addEventListener("blur", function () {
+  mailInp.classList.add("blur");
+});
+
 barIcon.addEventListener("click", function () {
   hamburgerMenu.style.transition = "all 0.5s";
   if (hamburgerMenu.classList.contains("active")) {
@@ -72,6 +79,39 @@ productIcon.forEach((item) => {
     }
   });
 });
+
+let errMsg = document.createElement("div");
+
+newsletterForm.addEventListener("submit", function (e) {
+  errMsg.className = "invalid-feedback";
+  if (mailInp.value === "") {
+    e.preventDefault();
+    errMsg.innerHTML = "Enter an email address!!";
+    errMsg.style.display = "block";
+    mailInp.style.borderColor = "#dc3545";
+    mailInp.parentElement.after(errMsg);
+    removeAlert();
+  } else if (!mailFormat.test(mailInp.value)) {
+    e.preventDefault();
+    errMsg.innerHTML = "Please write the e-mail address in the correct format";
+    errMsg.style.display = "block";
+    mailInp.style.borderColor = "#dc3545";
+    mailInp.parentElement.after(errMsg);
+    removeAlert();
+  }
+});
+
+function removeAlert() {
+  mailInp.addEventListener("keydown", function () {
+    errMsg.style.display = "none";
+    mailInp.style.borderColor = "#28a745";
+  });
+  mailInp.addEventListener("blur", function () {
+    if (mailInp.value === "") {
+      mailInp.style.borderColor = "";
+    }
+  });
+}
 
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
