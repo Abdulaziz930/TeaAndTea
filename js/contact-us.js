f93ffe3ea37c3cc5f63488aa4d4197d64e4551dd
Abdulaziz930 <abdulazizak@code.edu.az>
2021-03-14 02:10:34 +0400

    updated contact-us-page

--- js/contact-us.js
+++ js/contact-us.js
@@ -12,6 +12,7 @@ let lastBadge = document.querySelector(".last-list-item");
 let contactForm = document.querySelector("#contactForm  ");
 let emailInp = document.querySelector("input[type='email']");
 let textarea = document.querySelector("textarea");
+let mailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 window.addEventListener("scroll", function () {
   navbar.style.transition = "all 0.5s";
@@ -61,11 +62,13 @@ barIcon.addEventListener("click", function () {
 
 let errMsg = document.createElement("div");
 let secondErrMsg = document.createElement("div");
+let mailErrMsg = document.createElement("div");
 
 contactForm.addEventListener("submit", function (e) {
   e.preventDefault();
   errMsg.className = "invalid-feedback";
   secondErrMsg.className = "invalid-feedback";
+  mailErrMsg.className = "invalid-feedback";
 
   if (emailInp.value.trim() === "") {
     errMsg.innerHTML = "Enter an email address!!";
@@ -73,6 +76,13 @@ contactForm.addEventListener("submit", function (e) {
     emailInp.style.borderColor = "#dc3545";
     emailInp.parentElement.after(errMsg);
     removeAlert(emailInp, errMsg);
+  } else if (!mailFormat.test(emailInp.value)) {
+    mailErrMsg.innerHTML =
+      "Please write the e-mail address in the correct format";
+    mailErrMsg.style.display = "block";
+    emailInp.style.borderColor = "#dc3545";
+    emailInp.parentElement.after(mailErrMsg);
+    removeAlert(emailInp, mailErrMsg);
   }
   if (textarea.value.trim() === "") {
     secondErrMsg.innerHTML = "Enter an comment!!";
